f245c2e278713ee94752cf537fd19fe7affaf875
alewal <alewal@WLBCLIENT3>
2012-06-24 02:27:22 +0200

    Personal Greetings

--- Android Learning Example - Hello World/src/de/alxwlb/android/learning/HelloWorldActivity.java
+++ Android Learning Example - Hello World/src/de/alxwlb/android/learning/HelloWorldActivity.java
@@ -3,12 +3,10 @@ package de.alxwlb.android.learning;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class HelloWorldActivity extends Activity {
     
